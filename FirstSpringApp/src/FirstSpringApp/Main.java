ee9f9c0cb6520a641a1e965c4ea21e9c0f653fbd
BastaTo <Tomasz.Basta@ista.com>
2020-01-12 23:06:31 +0100

    Tests

--- FirstSpringApp/src/FirstSpringApp/Main.java
+++ FirstSpringApp/src/FirstSpringApp/Main.java
@@ -23,5 +23,7 @@ public class Main {
         System.out.println(cricketCoach.getDailyWorkout());
         System.out.println(cricketCoach.getName());
         System.out.println(cricketCoach.getTeam());
+
+        //dodany nowy komentarz
     }
 }
